d1c436bd66f38025a70698697dba37fee95122ff
Alina <malinka2002@mail.ru>
2022-05-20 23:56:00 +0500

    header

---
+++ lager/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Header.scss';
+
+export function Header() {
+    return (
+        <header className="header">
+            {/* <a href=""><img src="../public/img/bell.svg" alt="bell"/></a>
+            <a href=""><img src="../public/img/user.svg" alt="user"/></a> */}
+        </header>
+    )
+}
--- lager/src/pages/Home/Home.tsx
+++ lager/src/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Header from '../../Header';
+import { Header } from '../../components/Header';
 import { Events } from '../../components/Events';
 
 export function Home() {
   return (
     <div>    
-      <Header/>
+      <Header />
       <Events />
     </div>
   );
