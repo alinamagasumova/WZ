11ac961c3ff4c833a868c2b13623cc0244bcc8c5
dimav <volchukdmit@gmail.com>
2022-05-20 21:39:39 +0300

    events

--- lager/src/pages/Home/Home.tsx
+++ lager/src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { Events } from '../../components/Events';
+
 export function Home() {
   return (
     <div>
-      <p>Container</p>
+      <Events />
     </div>
   );
 }
