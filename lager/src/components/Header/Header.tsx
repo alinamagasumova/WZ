4c1a4648d0b2a3500d4cd598a2ce6752aa200a82
Alina <malinka2002@mail.ru>
2022-05-21 00:07:58 +0500

    header

--- lager/src/components/Header/Header.tsx
+++ lager/src/components/Header/Header.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import './Header.scss';
+// @ts-ignore
+import bell from './bell.svg';
+// @ts-ignore
+import user from './user.svg';
+
 
 export function Header() {
     return (
         <header className="header">
-            {/* <a href=""><img src="../public/img/bell.svg" alt="bell"/></a>
-            <a href=""><img src="../public/img/user.svg" alt="user"/></a> */}
+            <a href=""><img src={bell} alt="bell"/></a>
+            <a href=""><img src={user} alt="user"/></a>
         </header>
     )
 }
