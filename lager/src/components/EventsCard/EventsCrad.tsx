3805928cb82fe69a9fc1650e1353b7fa08eb0382
dimav <volchukdmit@gmail.com>
2022-05-20 21:47:49 +0300

    img exa,ple

---
+++ lager/src/components/EventsCard/EventsCrad.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+// @ts-ignore
+import coins from './coins.png';
+
+export function EventsCard() {
+  return (
+    <div className="eventsCard">
+      <div className="eventsCard__title">
+
+      </div>
+
+      <div>
+        <img src={coins} alt=""/>
+      </div>
+    </div>
+  )
+}
--- lager/src/pages/Home/Home.tsx
+++ lager/src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { Events } from '../../components/Events';
+import { EventsCard } from '../../components/EventsCard';
 
 export function Home() {
   return (
     <div>
       <Events />
+      <EventsCard />
     </div>
   );
 }
